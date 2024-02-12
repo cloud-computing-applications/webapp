c38037615b307884979678a4d32dea4f30efb66f
Parichay Didwania <parichaydidwania@gmail.com>
2024-02-12 15:59:03 -0500

    added database name as env variable

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const DB = {};
 
-DB.sequelize = new Sequelize('TESTDB', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+DB.sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false
