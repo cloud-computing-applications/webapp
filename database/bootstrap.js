019c4ed25816c01e7ce587f8ce74aa1e5f9ecb5c
Parichay Didwania <parichaydidwania@gmail.com>
2024-02-12 20:07:13 -0500

    seperated test db from main db

--- database/bootstrap.js
+++ database/bootstrap.js
@@ -1,11 +1,27 @@
+const { Sequelize } = require('sequelize');
 const DB = require('./db');
 const mockDB = require('./mock-db');
 
-module.exports = async (mock = false) => {
+module.exports = async (testDB = false, mock = false) => {
     if(mock) {
         mockDB(DB);
+    } else {
+        DB.sequelize = new Sequelize(testDB ? process.env.DB_TEST_DATABASE : process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+            host: process.env.DB_HOST,
+            dialect: 'mysql',
+            logging: false
+        });
+        
+        const models = [
+            require('../models/user')(DB.sequelize)
+        ];
+        
+        DB.models = {};
+        for(const model of models) {
+            DB.models[model.name] = model;
+        }
     }
-       
+
     await DB.sequelize.sync({ alter: true });
     await DB.sequelize.authenticate();
 }
