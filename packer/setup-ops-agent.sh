3c274e4461ac08842cff12ea4e205dbdec6d6858
Parichay Didwania <parichaydidwania@gmail.com>
2024-03-19 00:12:25 -0400

    adding ops agent config file via file provisioner

--- packer/setup-ops-agent.sh
+++ packer/setup-ops-agent.sh
@@ -8,29 +8,6 @@ sudo mkdir /var/log/webapp
 sudo chown -R csye6225:csye6225 /var/log/webapp
 sudo chmod -R 744 /var/log/webapp
 
-sudo /bin/bash -c "cat <<EOF > \"/etc/google-cloud-ops-agent/config.yaml\"
-logging:
-  receivers:
-    webapp-receiver:
-      type: files
-      include_paths:
-        - \"${LOG_FILE_PATH}\"
-      record_log_file_path: true
-  processors:
-    webapp-processor:
-      type: parse_json
-      time_key: timestamp
-      time_format: \"%Y-%m-%dT%H:%M:%S.%LZ\"
-    move_severity:
-      type: modify_fields
-      fields:
-        severity:
-          move_from: jsonPayload.level
-  service:
-    pipelines:
-      default_pipeline:
-        receivers: [webapp-receiver]
-        processors: [webapp-processor, move_severity]
-EOF"
-
+sudo yq e ".logging.receivers.\"webapp-receiver\".include_paths[0] = \"${LOG_FILE_PATH}\"" -i /tmp/config.yaml
+sudo mv /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
