00d9d426e53f7120827be074787c8233579f5216
Parichay Didwania <parichaydidwania@gmail.com>
2024-03-27 06:48:02 -0400

    log for inavtive user added

--- errorHandler.js
+++ errorHandler.js
@@ -46,7 +46,8 @@ const DATABASE_ERROR_TYPES = {
 }
 
 const AUTHENTICATION_ERROR_TYPES = {
-    AUTHENTICATION_UNAUTHORIZED: { message: "Invalid Basic Token", httpStatusCode: 401 }
+    AUTHENTICATION_UNAUTHORIZED: { message: "Invalid Basic Token", httpStatusCode: 401 },
+    AUTHENTICATION_INACTIVE: { message: "Email verification is required for the user", httpStatusCode: 401 }
 }
 
 const PUB_SUB_ERROR_TYPES = {
